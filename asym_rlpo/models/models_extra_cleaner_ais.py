d763bf5cadd717c675eccd73fdd13b56040b78a4
Amit Sinha <amitfishy@gmail.com>
2022-11-17 14:53:05 -0500

    Added carflag and cleaner with AIS

--- asym_rlpo/models/models_extra_cleaner_ais.py
+++ asym_rlpo/models/models_extra_cleaner_ais.py
@@ -2,12 +2,15 @@ import torch.nn as nn
 
 from asym_rlpo.envs import Environment
 from asym_rlpo.modules import make_module
-from asym_rlpo.representations.history import GRUHistoryRepresentation
+from asym_rlpo.representations.history_ais import GRUHistoryRepresentationAIS
 from asym_rlpo.representations.identity import IdentityRepresentation
 from asym_rlpo.representations.normalization import NormalizationRepresentation
 from asym_rlpo.representations.onehot import OneHotRepresentation
 from asym_rlpo.representations.resize import ResizeRepresentation
 from asym_rlpo.utils.config import get_config
+
+from gym import spaces
+import numpy as np
 
 
 def _make_q_model(in_size, out_size):
@@ -49,10 +52,12 @@ def _make_representation_models(env: Environment) -> nn.ModuleDict:
     # agent
     action_model = OneHotRepresentation(env.action_space)
     observation_model = IdentityRepresentation(env.observation_space)
+    reward_model = IdentityRepresentation(spaces.Box(np.array([-float("inf")]), np.array([float("inf")])))
     latent_model = IdentityRepresentation(env.latent_space)
-    history_model = GRUHistoryRepresentation(
+    history_model = GRUHistoryRepresentationAIS(
         action_model,
         observation_model,
+        reward_model,
         hidden_size=128,
     )
 
@@ -71,19 +76,79 @@ def _make_representation_models(env: Environment) -> nn.ModuleDict:
             'latent_model': latent_model,
             'action_model': action_model,
             'observation_model': observation_model,
+            'reward_model': reward_model,
             'history_model': history_model,
         }
     )
 
+def _make_h_ais_psi_model(history_dim, actions_dim):
+    ais_psi_base = nn.Sequential(
+        make_module('linear', 'relu', history_dim + actions_dim, history_dim),
+        nn.ReLU(),
+        make_module('linear', 'relu', history_dim, history_dim),
+        nn.ReLU(),
+    )
+
+    ais_psi_pred_rew = nn.Sequential(
+        make_module('linear', 'relu', history_dim, 1),
+        )
+    ais_psi_pred_next_ais = nn.Sequential(
+        make_module('linear', 'relu', history_dim, history_dim),
+        )
+
+    return {
+        'h_ais_psi_base': ais_psi_base,
+        'h_pred_rew': ais_psi_pred_rew,
+        'h_pred_next_ais': ais_psi_pred_next_ais,
+    }
+
+
+def _make_hz_ais_psi_model(latent_dim, history_dim, actions_dim):
+    vanilla_ais_psi_base = nn.Sequential(
+        make_module('linear', 'relu', latent_dim + history_dim + actions_dim, history_dim),
+        nn.ReLU(),
+        make_module('linear', 'relu', history_dim, history_dim),
+        nn.ReLU(),
+    )
+    asymac_ais_psi_base = nn.Sequential(
+        make_module('linear', 'relu', history_dim, history_dim),
+        nn.ReLU(),
+        make_module('linear', 'relu', history_dim, history_dim),
+        nn.ReLU(),
+    )
+
+    ais_psi_pred_rew = nn.Sequential(
+        make_module('linear', 'relu', history_dim, 1),
+        )
+    ais_psi_pred_next_ais = nn.Sequential(
+        make_module('linear', 'relu', history_dim, history_dim),
+        )
+
+    asymac_ais_psi_pred_latent = nn.Sequential(
+        make_module('linear', 'relu', history_dim, latent_dim),
+        )
+
+    return {
+        'hz_vanilla_ais_psi_base': vanilla_ais_psi_base,
+        'hz_asymac_ais_psi_base': asymac_ais_psi_base,
+        'hz_pred_rew': ais_psi_pred_rew,
+        'hz_pred_next_ais': ais_psi_pred_next_ais,
+        'hz_pred_latent': asymac_ais_psi_pred_latent,
+    }
 
 def make_models(env: Environment) -> nn.ModuleDict:
+    common_ais_representation = _make_representation_models(env)
 
     models = nn.ModuleDict(
         {
-            'agent': _make_representation_models(env),
-            'critic': _make_representation_models(env),
+            'agent': common_ais_representation,
+            'critic': common_ais_representation,
+            'ais_psi': common_ais_representation,
         }
     )
+
+    models.ais_psi.update(_make_h_ais_psi_model(models.agent.history_model.dim, models.agent.action_model.dim))
+    models.ais_psi.update(_make_hz_ais_psi_model(models.agent.latent_model.dim, models.agent.history_model.dim, models.agent.action_model.dim))
 
     # DQN models
     models.agent.update(
